ae359c1edf38615c1e1d8e65a862d02561082dd8
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-07-07 23:58:59 +0100

    change the style of table

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Hero from "./Hero";
-
+import coin from "../assets/turbo.png";
 import CoinItem from "./CoinItem";
 
 function AppLayout() {
+  const tabelHeader = ["coin", "price", "24h %"];
+  const tableData = [];
   return (
     <div className="bg-black">
       <Header />
@@ -11,20 +13,40 @@ function AppLayout() {
       <div className=" px-4 py-4">
         <div className=" max-w-7xl mx-auto">
           <p className="text-white text-xl font-semibold py-6">Crypto Prices</p>
-          <div>
-            <div className="bg-gradient-to-r from-blue-500 to-cyan-500 px-2 py-2 rounded-t-lg text-white font-semibold text-lg">
-              coin
-            </div>
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
-            <CoinItem />
+          <div className="rounded-t-lg overflow-hidden">
+            <table className=" w-full ">
+              <thead className="bg-gradient-to-r from-blue-500 to-cyan-500 text-white  text-lg ">
+                <tr className="">
+                  <th className="font-semibold py-2">coin</th>
+                  <th className="font-semibold w-24">price</th>
+                  <th className="font-semibold w-20 ">24h %</th>
+
+                  {/* <div className="flex bg-red-400">
+                  <th>price</th>
+                  <th>24h %</th>
+                </div> */}
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="p-2  border-b bg-yellow-400">
+                  <td className="flex py-2 items-center gap-3">
+                    <img src={coin} alt="coin" className="w-7 h-7" />
+                    <p className="text-white text-lg ">Bitcoin</p>
+                  </td>
+                  {/* <div className="flex  items-end justify-center gap-1 bg-green-500"> */}
+                  <td className="font-semibold py-2 w-24 text-center text-white">
+                    $69,423.04
+                  </td>
+                  <td className="w-20 py-2">
+                    <div className="text-white font-semibold text-sm bg-red-600 px-2 py-[2px] rounded-sm w-fit mx-auto">
+                      -3.28%
+                    </div>
+                  </td>
+                  {/* </div> */}
+                </tr>
+              </tbody>
+              {/* <CoinItem /> */}
+            </table>
           </div>
         </div>
       </div>
--- src/ui/CoinItem.jsx
+++ src/ui/CoinItem.jsx
@@ -3,13 +3,13 @@ import coin from "../assets/turbo.png";
 function CoinItem() {
   return (
     <div className=" px-2 py-2 flex justify-between items-center border-b">
-      <div className="flex items-center gap-3">
+      <div className="flex bg-yellow-300 items-center gap-3">
         <img src={coin} alt="coin" className="w-8 h-8" />
-        <p className="text-white text-lg">Bitcoin</p>
+        <p className="text-white text-lg ">Bitcoin</p>
       </div>
-      <div className="flex flex-col items-end justify-center gap-1">
-        <div className="font-semibold text-white">$69,423.04</div>
-        <div className="text-white font-semibold text-sm bg-red-600 w-fit px-2 py-[2px] rounded-sm">
+      <div className="flex  items-end justify-center gap-1 bg-green-500">
+        <div className="font-semibold text-white w-">$69,423.04</div>
+        <div className="text-white font-semibold text-sm bg-red-600 px-2 py-[2px] rounded-sm w-1/3">
           -3.28%
         </div>
       </div>
