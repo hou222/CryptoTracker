d8ab56983b55aebb3ff79af062828648e47dfa3a
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-07-21 22:59:22 +0100

    add chart

--- src/pages/CoinDetails.jsx
+++ src/pages/CoinDetails.jsx
@@ -1,10 +1,41 @@
 import { useEffect, useState } from "react";
+import Chart from "chart.js/auto";
 import { useParams } from "react-router-dom";
+import { Line } from "react-chartjs-2";
 //import parse from "html-react-parser";
 function CoinDetails() {
   const [details, setDetails] = useState([]);
   const { id } = useParams();
   const url = `https://api.coingecko.com/api/v3/coins/${id}`;
+
+  const data = {
+    labels: [
+      "10:22",
+      "10:23",
+      "10:11",
+      "10:23",
+      "10:11",
+      "10:11",
+      "10:23",
+      "10:11",
+    ],
+    datasets: [
+      {
+        label: "price",
+        data: [
+          "2000",
+          "4000",
+          "2342",
+          "5234",
+          "2342",
+          "5234",
+          "2000",
+          "5234",
+          "2342",
+        ],
+      },
+    ],
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -23,6 +54,7 @@ function CoinDetails() {
         <img src={details.image?.large} className="w-[180px] h-[180px]" />
         <p className="text-5xl font-bold">BNB</p>
       </div>
+
       <div
         className="py-3 h-[100px] overflow-y-scroll "
         dangerouslySetInnerHTML={{
@@ -36,7 +68,10 @@ function CoinDetails() {
       <p className="font-bold text-2xl py-2">
         Market Cap: <span className="font-normal">$0,000,000M</span>
       </p>
-      <div className="bg-blue-400 w-full h-[150px] my-6"></div>
+      {/* <Doughnut data={...}/> */}
+      <div className=" w-full bg-red-400 h-[175px] my-6">
+        <Line data={data}></Line>
+      </div>
     </div>
   );
 }
