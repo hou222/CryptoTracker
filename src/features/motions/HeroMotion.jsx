8d72534a54660d255b941ea587a02ebb1400d58a
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-08-05 18:11:11 +0100

    add parallax effect

--- src/features/motions/HeroMotion.jsx
+++ src/features/motions/HeroMotion.jsx
@@ -1,14 +1,81 @@
 import img from "../../assets/seaBlue.png";
-import img2 from "../../assets/turbo.png";
+//import img2 from "../../assets/turbo.png";
+import binance from "../../assets/binance.png";
+import uniswap from "../../assets/uniswap.png";
+import bitcoin from "../../assets/bitcoin.png";
+import ethereum from "../../assets/ethereum.png";
+import usdc from "../../assets/usdc.png";
+import usdt from "../../assets/usdt.png";
+import polygon from "../../assets/polygon.png";
+import solana from "../../assets/solana.png";
+import tron from "../../assets/tron.png";
 import { motion } from "framer-motion";
 function HeroMotion({ rotate, y }) {
   return (
     <>
       <motion.img
-        style={{ y, rotate }}
+        style={{ rotate, y }}
+        src={ethereum}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-48 left-14 md:top-7 md:left-24 lg:left-36  md:w-16 md:h-16 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={solana}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-32 left-0 md:top-40 md:w-12 md:h-12 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={tron}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-20 left-12 md:top-64 md:left-24 lg:left-32 md:w-11 md:h-11 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={usdt}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-18 left-0 md:top-[370px] md:w-12 md:h-12 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={usdc}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute top-16 left-12 md:top-[480px] md:left-24 lg:left-36 md:w-12 md:h-12 z-20"
+      />
+      {/* ///////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////// */}
+      <motion.img
+        style={{ rotate, y }}
+        src={bitcoin}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-48 right-14 md:top-7 md:right-24 lg:right-36  md:w-12 md:h-12 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={polygon}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-32 right-0 md:top-40 md:w-16 md:h-16 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
         src={img}
         alt="coin"
-        className="w-96 h-96 absolute top-60 left-3 lg:w-14 lg:h-14     z-20"
+        className="w-[14px] h-[14px] absolute -top-20 right-12 md:top-64 md:right-24 lg:right-32 md:w-11 md:h-11 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={uniswap}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute -top-18 right-0 md:top-[370px] md:w-12 md:h-12 z-20"
+      />
+      <motion.img
+        style={{ rotate, y }}
+        src={binance}
+        alt="coin"
+        className="w-[14px] h-[14px] absolute top-16 right-12 md:top-[480px] md:right-24 lg:right-36 md:w-12 md:h-12 z-20"
       />
       {/* <motion.img
         style={{ y, rotate }}
--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -3,22 +3,24 @@ import HeroMotion from "../features/motions/HeroMotion";
 import { useScroll, useTransform } from "framer-motion";
 function Hero() {
   const targetRef = useRef(null);
-  const { scrollYProgress } = useScroll({
+  const { scrollYProgress, scrollX } = useScroll({
     target: targetRef,
     offset: ["start end", "end start"],
   });
   const rotate = useTransform(scrollYProgress, [0, 1], ["0deg", "3000deg"]);
-  const y = useTransform(scrollYProgress, [0, 1], [0, 1000]);
+  const y = useTransform(scrollYProgress, [0, 1], [-800, 1000]);
+  const x = useTransform(scrollX, [0, 1], [-300, 300]);
+
   return (
     <div className="md:pt-14">
       <div
         ref={targetRef}
-        className=" mx-6 flex relative flex-col items-center text-white overflow-hidden "
+        className=" mx-6 flex relative flex-col items-center text-white overflow-hidden max-w-7xl xl:mx-auto"
       >
-        <HeroMotion rotate={rotate} y={y} />
+        <HeroMotion rotate={rotate} y={y} x={x} />
 
         <div className="flex flex-col  gap-4 md:gap-10 items-center  py-10 md:py-16 w-60 md:w-[650px] z-30 ">
-          <div className="bg-blue-500 blur-[70px] md:blur-[90px]  w-[250px] h-[35px]  absolute top-20 md:top-40 md:w-[600px] md:h-[100px]"></div>
+          <div className="bg-blue-500 blur-[70px] md:blur-[90px]  w-[200px] h-[30px]  absolute top-16 md:top-40 md:w-[600px] md:h-[100px]"></div>
           <p
             className=" w-[200px] md:w-[650px] md:py-5 md:filter-none top-8 text-center font-bold text-3xl md:text-[96px] md:leading-[110px] z-30 
     "
